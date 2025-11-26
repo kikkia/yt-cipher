a179f23b2164433a8d092333376efcfad99156ed
tcely <tcely@users.noreply.github.com>
2025-11-26 05:31:38 -0500

    Update imports to use jsr in server.ts

--- server.ts
+++ server.ts
@@ -1,4 +1,4 @@
-import { serve } from "https://deno.land/std@0.224.0/http/server.ts";
+import { serve } from "jsr:@std/http@0.224.0";
 import { initializeWorkers } from "./src/workerPool.ts";
 import { initializeCache } from "./src/playerCache.ts";
 import { handleDecryptSignature } from "./src/handlers/decryptSignature.ts";
